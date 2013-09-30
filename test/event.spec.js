f03bd7338915ffe379d8645179be8763465f1e52
Szymon WYgnański <s@finalclass.net>
2013-09-30 15:22:17 +0200

    more tests for stopPropagation

--- test/event.spec.js
+++ test/event.spec.js
@@ -12,7 +12,7 @@ describe('final-events capture target bubbling', function () {
     child = dispatcher({id: 'child', parent: parent});
   });
 
-  it('can stop propagation on capturing phase', function () {
+  it('can stop propagation in capturing phase', function () {
     var onRootCaptureSpy = jasmine.createSpy();
     var onParentCaptureSpy = jasmine.createSpy();
     var onTargetSpy = jasmine.createSpy();
@@ -35,6 +35,56 @@ describe('final-events capture target bubbling', function () {
     expect(onParentCaptureSpy).not.toHaveBeenCalled();
     expect(onTargetSpy).not.toHaveBeenCalled();
     expect(onParentBubbleSpy).not.toHaveBeenCalled();
+    expect(onRootBubbleSpy).not.toHaveBeenCalled();
+  });
+
+  it('can stop propagation in target phase', function () {
+    var onRootCaptureSpy = jasmine.createSpy();
+    var onParentCaptureSpy = jasmine.createSpy();
+    var onTargetSpy = jasmine.createSpy();
+    var onParentBubbleSpy = jasmine.createSpy();
+    var onRootBubbleSpy = jasmine.createSpy();
+
+    root.on('test', onRootCaptureSpy, true); //this should be called
+    parent.on('test', onParentCaptureSpy, true);
+    child.on('test', function (event) {
+        event.stopPropagation();
+    });
+    child.on('test', onTargetSpy);
+    parent.on('test', onParentBubbleSpy);
+    root.on('test', onRootBubbleSpy);
+
+    child.trigger('test');
+
+    expect(onRootCaptureSpy).toHaveBeenCalled();
+    expect(onParentCaptureSpy).toHaveBeenCalled();
+    expect(onTargetSpy).toHaveBeenCalled();
+    expect(onParentBubbleSpy).not.toHaveBeenCalled();
+    expect(onRootBubbleSpy).not.toHaveBeenCalled();
+  });
+
+  it('can stop propagation in target phase', function () {
+    var onRootCaptureSpy = jasmine.createSpy();
+    var onParentCaptureSpy = jasmine.createSpy();
+    var onTargetSpy = jasmine.createSpy();
+    var onParentBubbleSpy = jasmine.createSpy();
+    var onRootBubbleSpy = jasmine.createSpy();
+
+    root.on('test', onRootCaptureSpy, true); //this should be called
+    parent.on('test', onParentCaptureSpy, true);
+    child.on('test', onTargetSpy);
+    parent.on('test', function (event) {
+      event.stopPropagation();
+    });
+    parent.on('test', onParentBubbleSpy);
+    root.on('test', onRootBubbleSpy);
+
+    child.trigger('test');
+
+    expect(onRootCaptureSpy).toHaveBeenCalled();
+    expect(onParentCaptureSpy).toHaveBeenCalled();
+    expect(onTargetSpy).toHaveBeenCalled();
+    expect(onParentBubbleSpy).toHaveBeenCalled();
     expect(onRootBubbleSpy).not.toHaveBeenCalled();
   });
 
